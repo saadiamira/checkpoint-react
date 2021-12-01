22d91a2a4bdb6f0ac95ac6b71b65ff90a1700711
saadi amira <amirasaadi754@gmail.com>
2021-12-01 10:27:50 +0100

    first commit

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import"./Footer.css";
+export default function Footer() {
+    return (
+        <div>
+            <div>
+                <p>COPYrithe 2021</p>
+            </div>
+        </div>
+    )
+}
